e7a2b562e17f9b59c55cbf2c826088baf7c80706
Eduardo Inerarte <ei@schilling.dk>
2024-07-04 14:58:35 +0200

    refactor: Update Table component imports

--- src/components/Table/index.ts
+++
@@ -1,7 +0,0 @@
-import table from './Table'
-import TableHeaders from './TableHeaders'
-import TableContent from './TableContent'
-import TableFooter from './TableFooter'
-
-
-export {TableFooter, TableHeaders, TableContent, table}
